1e7e9d2d4fb1fc0c80f49dff2b1b4b1069110a3a
KiranMantha <kiranv.mantha@gmail.com>
2025-08-24 15:07:19 +0530

    Updated secret and host to new database

--- routes/supabase_client.py
+++ routes/supabase_client.py
@@ -1,7 +1,7 @@
 from supabase import create_client
 
-DB_URL = 'https://vbxaqvyvmjpxogaenvdy.supabase.co'
-SECRET = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZieGFxdnl2bWpweG9nYWVudmR5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzYzMzI1OTEsImV4cCI6MjA1MTkwODU5MX0.1SqsgxgrKTsZMuGr6oDPhULjjjPzjPpaQRy1L1ZFZWU'
+DB_URL = 'https://tbobimigdddygwhenclp.supabase.co'
+SECRET = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRib2JpbWlnZGRkeWd3aGVuY2xwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYwMDcyNjEsImV4cCI6MjA3MTU4MzI2MX0.HDkogP6nZepcYRKr-ZE0605RHODGARjPWVVUOy06XUU'
 
 supabase = create_client(DB_URL, SECRET)
 
